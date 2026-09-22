b30de7b7e42287484bb339b119e3a4226f752a20
Intelligencecore <07wood.temblor@icloud.com>
2026-09-21 23:09:45 -0400

    Push Xcode Files

---
+++ financialApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  financialApp
+//
+//  Created by Richier on 9/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ financialApp/financialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financialAppApp.swift
+//  financialApp
+//
+//  Created by Richier on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct financialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
